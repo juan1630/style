189b7df8801550c59463b246921743e156b4795c
Juan <josejuanpatron1630@gmail.com>
2019-01-22 14:32:30 -0600

    Agregamos el ciclo de vida de los componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck,AfterContentInit ,AfterContentChecked, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'miselaneos';
+
+  constructor(){ 
+    console.log("Constructor")
+   }
+
+   ngOnChanges(){
+    console.log("ngOnChanges")
+ }
+
+   ngOnInit(){
+     console.log("ngOnInit")
+   }
+   ngDoCheck(){
+      console.log("ngDoCheck");
+   }
+   ngAfterContentInit(){
+     console.log("ngAfterContentInit")
+   }
+
+   ngAfterContentChecked(){
+     console.log("ngAfterContentChecked")
+   }
+   ngAfterViewInit(){
+     console.log("ngAfterViewInit");
+   }
+
+   ngAfterViewChecked(){
+     console.log("ngAfterViewChecked")
+   }
+
+   ngOnDestroy(){
+     console.log("ngOnDestroy")
+   }
 }
